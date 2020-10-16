ed8f01e16d6d57f65f62b4ff79c8acdbfcc927b4
iulianpro <iulian.prodea@gmail.com>
2020-10-16 18:16:48 +0100

    added products app, models and fixtures

---
+++ products/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(
+        'Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description_1 = models.CharField(max_length=254)
+    description_2 = models.CharField(max_length=254)
+    description_3 = models.CharField(max_length=254)
+    description_4 = models.CharField(max_length=254)
+    description_5 = models.CharField(max_length=254)
+    description_6 = models.CharField(max_length=254)
+    description_7 = models.CharField(max_length=254)
+    description_8 = models.CharField(max_length=254)
+    description_9 = models.CharField(max_length=254)
+    description_10 = models.CharField(max_length=254)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(
+        max_digits=6, decimal_places=2, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return '{0} ({1})'.format(self.name, self.category.friendly_name)
