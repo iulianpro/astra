60480e0304656dfd7bcd5823d3a9ff852e09b505
iulianpro <iulian.prodea@gmail.com>
2020-10-21 19:23:24 +0100

    added profile to admin backend

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import UserProfile
 
-# Register your models here.
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'default_phone_number',
+        'default_country',
+        'default_town_or_city',
+        'default_app',
+        'default_mac',
+    )
+
+    ordering = ('user',)
+
+
+admin.site.register(UserProfile, ProfileAdmin)
