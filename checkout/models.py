4c43fe84b4efe928806cadd1b585a93377a51c2b
iulianpro <iulian.prodea@gmail.com>
2020-10-22 16:12:45 +0100

    created model in checkout app

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,46 @@
+import uuid
 from django.db import models
+from django.conf import settings
+from django_countries.fields import CountryField
 
-# Create your models here.
+
+class Order(models.Model):
+    APP_CHOICE = [
+        ('select', 'Selecteaza aplicatia'),
+        ('smart_iptv', 'Smart IPTV'),
+        ('set_iptv', 'Set IPTV'),
+        ('net_iptv', 'Net IPTV'),
+        ('iptv_extreme', 'IPTV Extreme'),
+        ('mag_device', 'Dispozitiv MAG'),
+        ('stb_device', 'Dispozitiv STB'),
+        ('another_device', 'Alt dispozitiv'),
+    ]
+    order_number = models.CharField(max_length=32, null=False, editable=False)
+    full_name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    phone_number = models.CharField(max_length=20, null=True, blank=True)
+    country = CountryField(blank_label='Country', null=False, blank=False)
+    town_or_city = models.CharField(max_length=40, null=False, blank=False)
+    app = models.CharField(max_length=40, choices=APP_CHOICE, default='select')
+    mac = models.CharField(max_length=40, null=False, blank=False)
+    mac_pass = models.CharField(max_length=40, null=True, blank=True)
+    notes = models.CharField(max_length=40, null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def _generate_order_number(self):
+        """
+        Generate a random, unique order number using UUID
+        """
+        return uuid.uuid4().hex.upper()
+
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the order number
+        if it hasn't been set already.
+        """
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.order_number
