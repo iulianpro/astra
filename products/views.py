9d707b5fa0d50a293e65c03f5ffda7292dbed2d5
iulianpro <iulian.prodea@gmail.com>
2020-10-29 18:09:52 +0000

    added dinamic buttons to product template

--- products/views.py
+++ products/views.py
@@ -1,16 +1,24 @@
 from django.shortcuts import render
+from profiles.models import UserProfile
 from .models import Product
 
 # Create your views here.
 
 
 def all_products(request):
-    """ A view to show all products, including sorting and search queries """
+    if request.user.is_authenticated:
+        profile = UserProfile.objects.get(user=request.user)
+        customer_id = profile.default_stripe_id
 
-    products = Product.objects.all()
-
-    context = {
-        'products': products,
-    }
-
-    return render(request, 'products/products.html', context)
+        products = Product.objects.all()
+        context = {
+            'products': products,
+            'customer_id': customer_id,
+        }
+        return render(request, 'products/products.html', context)
+    else:
+        products = Product.objects.all()
+        context = {
+            'products': products,
+        }
+        return render(request, 'products/products.html', context)
