0ccb4adbfd59f2826590e644b6ee9c169ff4ea17
iulianpro <iulian.prodea@gmail.com>
2020-10-31 15:49:13 +0000

    enhancement content and style subscription template

--- checkout/static/checkout/js/subscription.js
+++ checkout/static/checkout/js/subscription.js
@@ -10,11 +10,13 @@ $(document).ready(function () {
             $("#checkbox_2, #checkbox_3, #checkbox_4, #checkbox_5, #checkbox_6, #checkbox_7, #checkbox_8").attr("disabled", "disabled");
             $('#subscription_choice').text('Abonament 1 luna pe 1 dispozitiv:');
             $("#price_1, #price_2").text('€12.00 / luna');
+            $("#card_1").removeClass("card-subscription").addClass("card-subscription-selected");
         } else {
             $("#submit-subscription_1").attr("disabled", "disabled");
             $("#checkbox_2, #checkbox_3, #checkbox_4, #checkbox_5, #checkbox_6, #checkbox_7, #checkbox_8").removeAttr("disabled");
             $('#subscription_choice').text('Selecteaza un abonament:');
             $("#price_1, #price_2").text('€0.00 / luna');
+            $("#card_1").removeClass("card-subscription-selected").addClass("card-subscription");
         }
     });
 
@@ -25,11 +27,13 @@ $(document).ready(function () {
             $("#checkbox_1, #checkbox_3, #checkbox_4, #checkbox_5, #checkbox_6, #checkbox_7, #checkbox_8").attr("disabled", "disabled");
             $('#subscription_choice').text('Abonament 3 luni pe 1 dispozitiv:');
             $("#price_1, #price_2").text('€25.00 / luna');
+            $("#card_2").removeClass("card-subscription").addClass("card-subscription-selected");
         } else {
             $("#submit-subscription_2").attr("disabled", "disabled");
             $("#checkbox_1, #checkbox_3, #checkbox_4, #checkbox_5, #checkbox_6, #checkbox_7, #checkbox_8").removeAttr("disabled");
             $('#subscription_choice').text('Selecteaza un abonament:');
             $("#price_1, #price_2").text('€0.00 / luna');
+            $("#card_2").removeClass("card-subscription-selected").addClass("card-subscription");
         }
     });
 
@@ -40,11 +44,13 @@ $(document).ready(function () {
             $("#checkbox_2, #checkbox_1, #checkbox_4, #checkbox_5, #checkbox_6, #checkbox_7, #checkbox_8").attr("disabled", "disabled");
             $('#subscription_choice').text('Abonament 6 luni pe 1 dispozitiv:');
             $("#price_1, #price_2").text('€50.00 / luna');
+            $("#card_3").removeClass("card-subscription").addClass("card-subscription-selected");
         } else {
             $("#submit-subscription_3").attr("disabled", "disabled");
             $("#checkbox_2, #checkbox_1, #checkbox_4, #checkbox_5, #checkbox_6, #checkbox_7, #checkbox_8").removeAttr("disabled");
             $('#subscription_choice').text('Selecteaza un abonament:');
             $("#price_1, #price_2").text('€0.00 / luna');
+            $("#card_3").removeClass("card-subscription-selected").addClass("card-subscription");
         }
     });
 
@@ -55,11 +61,13 @@ $(document).ready(function () {
             $("#checkbox_2, #checkbox_3, #checkbox_1, #checkbox_5, #checkbox_6, #checkbox_7, #checkbox_8").attr("disabled", "disabled");
             $('#subscription_choice').text('Abonament 12 luni pe 1 dispozitiv:');
             $("#price_1, #price_2").text('€96.00 / luna');
+            $("#card_4").removeClass("card-subscription").addClass("card-subscription-selected");
         } else {
             $("#submit-subscription_4").attr("disabled", "disabled");
             $("#checkbox_2, #checkbox_3, #checkbox_1, #checkbox_5, #checkbox_6, #checkbox_7, #checkbox_8").removeAttr("disabled");
             $('#subscription_choice').text('Selecteaza un abonament:');
             $("#price_1, #price_2").text('€0.00 / luna');
+            $("#card_4").removeClass("card-subscription-selected").addClass("card-subscription");
         }
     });
 
@@ -70,11 +78,13 @@ $(document).ready(function () {
             $("#checkbox_2, #checkbox_3, #checkbox_4, #checkbox_1, #checkbox_6, #checkbox_7, #checkbox_8").attr("disabled", "disabled");
             $('#subscription_choice').text('Abonament 1 luna pe 2+ dispozitive:');
             $("#price_1, #price_2").text('€15.00 / luna');
+            $("#card_5").removeClass("card-subscription").addClass("card-subscription-selected");
         } else {
             $("#submit-subscription_5").attr("disabled", "disabled");
             $("#checkbox_2, #checkbox_3, #checkbox_4, #checkbox_1, #checkbox_6, #checkbox_7, #checkbox_8").removeAttr("disabled");
             $('#subscription_choice').text('Selecteaza un abonament:');
             $("#price_1, #price_2").text('€0.00 / luna');
+            $("#card_5").removeClass("card-subscription-selected").addClass("card-subscription");
         }
     });
 
@@ -85,11 +95,13 @@ $(document).ready(function () {
             $("#checkbox_2, #checkbox_3, #checkbox_4, #checkbox_5, #checkbox_1, #checkbox_7, #checkbox_8").attr("disabled", "disabled");
             $('#subscription_choice').text('Abonament 3 luni pe 2+ dispozitive:');
             $("#price_1, #price_2").text('€36.00 / luna');
+            $("#card_6").removeClass("card-subscription").addClass("card-subscription-selected");
         } else {
             $("#submit-subscription_6").attr("disabled", "disabled");
             $("#checkbox_2, #checkbox_3, #checkbox_4, #checkbox_5, #checkbox_1, #checkbox_7, #checkbox_8").removeAttr("disabled");
             $('#subscription_choice').text('Selecteaza un abonament:');
             $("#price_1, #price_2").text('€0.00 / luna');
+            $("#card_6").removeClass("card-subscription-selected").addClass("card-subscription");
         }
     });
 
@@ -100,11 +112,13 @@ $(document).ready(function () {
             $("#checkbox_2, #checkbox_3, #checkbox_4, #checkbox_5, #checkbox_6, #checkbox_1, #checkbox_8").attr("disabled", "disabled");
             $('#subscription_choice').text('Abonament 6 luni pe 2+ dispozitive:');
             $("#price_1, #price_2").text('€72.00 / luna');
+            $("#card_7").removeClass("card-subscription").addClass("card-subscription-selected");
         } else {
             $("#submit-subscription_7").attr("disabled", "disabled");
             $("#checkbox_2, #checkbox_3, #checkbox_4, #checkbox_5, #checkbox_6, #checkbox_1, #checkbox_8").removeAttr("disabled");
             $('#subscription_choice').text('Selecteaza un abonament:');
             $("#price_1, #price_2").text('€0.00 / luna');
+            $("#card_7").removeClass("card-subscription-selected").addClass("card-subscription");
         }
     });
 
@@ -115,11 +129,13 @@ $(document).ready(function () {
             $("#checkbox_2, #checkbox_3, #checkbox_4, #checkbox_5, #checkbox_6, #checkbox_7, #checkbox_1").attr("disabled", "disabled");
             $('#subscription_choice').text('Abonament 12 luni pe 2+ dispozitive:');
             $("#price_1, #price_2").text('€120.00 / luna');
+            $("#card_8").removeClass("card-subscription").addClass("card-subscription-selected");
         } else {
             $("#submit-subscription_8").attr("disabled", "disabled");
             $("#checkbox_2, #checkbox_3, #checkbox_4, #checkbox_5, #checkbox_6, #checkbox_7, #checkbox_1").removeAttr("disabled");
             $('#subscription_choice').text('Selecteaza un abonament:');
             $("#price_1, #price_2").text('€0.00 / luna');
+            $("#card_8").removeClass("card-subscription-selected").addClass("card-subscription");
         }
     });
 });
