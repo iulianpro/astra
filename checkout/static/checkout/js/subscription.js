a3d3393d2f1e37a84b65b6839cf914bb76c5586b
iulianpro <iulian.prodea@gmail.com>
2020-11-26 21:03:28 +0000

    improved ux on activate subscription

--- checkout/static/checkout/js/subscription.js
+++ checkout/static/checkout/js/subscription.js
@@ -8,12 +8,14 @@ $(document).ready(function () {
         if (checked_status == true) {
             $("#submit-subscription_1").removeAttr("disabled");
             $("#checkbox_2, #checkbox_3, #checkbox_4, #checkbox_5, #checkbox_6, #checkbox_7, #checkbox_8").attr("disabled", "disabled");
+            $("#checkbox_2, #checkbox_3, #checkbox_4, #checkbox_5, #checkbox_6, #checkbox_7, #checkbox_8").css('cursor', 'not-allowed');
             $('#subscription_choice').text('Abonament 1 luna pe 1 dispozitiv:');
             $("#price_1, #price_2").text('€12.00 / luna');
             $("#card_1").removeClass("card-subscription").addClass("card-subscription-selected");
         } else {
             $("#submit-subscription_1").attr("disabled", "disabled");
             $("#checkbox_2, #checkbox_3, #checkbox_4, #checkbox_5, #checkbox_6, #checkbox_7, #checkbox_8").removeAttr("disabled");
+            $("#checkbox_2, #checkbox_3, #checkbox_4, #checkbox_5, #checkbox_6, #checkbox_7, #checkbox_8").css('cursor', 'pointer');
             $('#subscription_choice').text('Selecteaza un abonament:');
             $("#price_1, #price_2").text('€0.00 / luna');
             $("#card_1").removeClass("card-subscription-selected").addClass("card-subscription");
@@ -25,12 +27,14 @@ $(document).ready(function () {
         if (checked_status == true) {
             $("#submit-subscription_2").removeAttr("disabled");
             $("#checkbox_1, #checkbox_3, #checkbox_4, #checkbox_5, #checkbox_6, #checkbox_7, #checkbox_8").attr("disabled", "disabled");
+            $("#checkbox_1, #checkbox_3, #checkbox_4, #checkbox_5, #checkbox_6, #checkbox_7, #checkbox_8").css('cursor', 'not-allowed');
             $('#subscription_choice').text('Abonament 3 luni pe 1 dispozitiv:');
             $("#price_1, #price_2").text('€25.00 / 3 luni');
             $("#card_2").removeClass("card-subscription").addClass("card-subscription-selected");
         } else {
             $("#submit-subscription_2").attr("disabled", "disabled");
             $("#checkbox_1, #checkbox_3, #checkbox_4, #checkbox_5, #checkbox_6, #checkbox_7, #checkbox_8").removeAttr("disabled");
+            $("#checkbox_1, #checkbox_3, #checkbox_4, #checkbox_5, #checkbox_6, #checkbox_7, #checkbox_8").css('cursor', 'pointer');
             $('#subscription_choice').text('Selecteaza un abonament:');
             $("#price_1, #price_2").text('€0.00 / luna');
             $("#card_2").removeClass("card-subscription-selected").addClass("card-subscription");
@@ -42,12 +46,14 @@ $(document).ready(function () {
         if (checked_status == true) {
             $("#submit-subscription_3").removeAttr("disabled");
             $("#checkbox_2, #checkbox_1, #checkbox_4, #checkbox_5, #checkbox_6, #checkbox_7, #checkbox_8").attr("disabled", "disabled");
+            $("#checkbox_2, #checkbox_1, #checkbox_4, #checkbox_5, #checkbox_6, #checkbox_7, #checkbox_8").css('cursor', 'not-allowed');
             $('#subscription_choice').text('Abonament 6 luni pe 1 dispozitiv:');
             $("#price_1, #price_2").text('€50.00 / 6 luni');
             $("#card_3").removeClass("card-subscription").addClass("card-subscription-selected");
         } else {
             $("#submit-subscription_3").attr("disabled", "disabled");
             $("#checkbox_2, #checkbox_1, #checkbox_4, #checkbox_5, #checkbox_6, #checkbox_7, #checkbox_8").removeAttr("disabled");
+            $("#checkbox_2, #checkbox_1, #checkbox_4, #checkbox_5, #checkbox_6, #checkbox_7, #checkbox_8").css('cursor', 'pointer');
             $('#subscription_choice').text('Selecteaza un abonament:');
             $("#price_1, #price_2").text('€0.00 / luna');
             $("#card_3").removeClass("card-subscription-selected").addClass("card-subscription");
@@ -59,12 +65,14 @@ $(document).ready(function () {
         if (checked_status == true) {
             $("#submit-subscription_4").removeAttr("disabled");
             $("#checkbox_2, #checkbox_3, #checkbox_1, #checkbox_5, #checkbox_6, #checkbox_7, #checkbox_8").attr("disabled", "disabled");
+            $("#checkbox_2, #checkbox_3, #checkbox_1, #checkbox_5, #checkbox_6, #checkbox_7, #checkbox_8").css('cursor', 'not-allowed');
             $('#subscription_choice').text('Abonament 12 luni pe 1 dispozitiv:');
             $("#price_1, #price_2").text('€96.00 / 12 luni');
             $("#card_4").removeClass("card-subscription").addClass("card-subscription-selected");
         } else {
             $("#submit-subscription_4").attr("disabled", "disabled");
             $("#checkbox_2, #checkbox_3, #checkbox_1, #checkbox_5, #checkbox_6, #checkbox_7, #checkbox_8").removeAttr("disabled");
+            $("#checkbox_2, #checkbox_3, #checkbox_1, #checkbox_5, #checkbox_6, #checkbox_7, #checkbox_8").css('cursor', 'pointer');
             $('#subscription_choice').text('Selecteaza un abonament:');
             $("#price_1, #price_2").text('€0.00 / luna');
             $("#card_4").removeClass("card-subscription-selected").addClass("card-subscription");
@@ -76,12 +84,14 @@ $(document).ready(function () {
         if (checked_status == true) {
             $("#submit-subscription_5").removeAttr("disabled");
             $("#checkbox_2, #checkbox_3, #checkbox_4, #checkbox_1, #checkbox_6, #checkbox_7, #checkbox_8").attr("disabled", "disabled");
+            $("#checkbox_2, #checkbox_3, #checkbox_4, #checkbox_1, #checkbox_6, #checkbox_7, #checkbox_8").css('cursor', 'not-allowed');
             $('#subscription_choice').text('Abonament 1 luna pe 2+ dispozitive:');
             $("#price_1, #price_2").text('€15.00 / luna');
             $("#card_5").removeClass("card-subscription").addClass("card-subscription-selected");
         } else {
             $("#submit-subscription_5").attr("disabled", "disabled");
             $("#checkbox_2, #checkbox_3, #checkbox_4, #checkbox_1, #checkbox_6, #checkbox_7, #checkbox_8").removeAttr("disabled");
+            $("#checkbox_2, #checkbox_3, #checkbox_4, #checkbox_1, #checkbox_6, #checkbox_7, #checkbox_8").css('cursor', 'pointer');
             $('#subscription_choice').text('Selecteaza un abonament:');
             $("#price_1, #price_2").text('€0.00 / luna');
             $("#card_5").removeClass("card-subscription-selected").addClass("card-subscription");
@@ -93,12 +103,14 @@ $(document).ready(function () {
         if (checked_status == true) {
             $("#submit-subscription_6").removeAttr("disabled");
             $("#checkbox_2, #checkbox_3, #checkbox_4, #checkbox_5, #checkbox_1, #checkbox_7, #checkbox_8").attr("disabled", "disabled");
+            $("#checkbox_2, #checkbox_3, #checkbox_4, #checkbox_5, #checkbox_1, #checkbox_7, #checkbox_8").css('cursor', 'not-allowed');
             $('#subscription_choice').text('Abonament 3 luni pe 2+ dispozitive:');
             $("#price_1, #price_2").text('€36.00 / 3 luni');
             $("#card_6").removeClass("card-subscription").addClass("card-subscription-selected");
         } else {
             $("#submit-subscription_6").attr("disabled", "disabled");
             $("#checkbox_2, #checkbox_3, #checkbox_4, #checkbox_5, #checkbox_1, #checkbox_7, #checkbox_8").removeAttr("disabled");
+            $("#checkbox_2, #checkbox_3, #checkbox_4, #checkbox_5, #checkbox_1, #checkbox_7, #checkbox_8").css('cursor', 'pointer');
             $('#subscription_choice').text('Selecteaza un abonament:');
             $("#price_1, #price_2").text('€0.00 / luna');
             $("#card_6").removeClass("card-subscription-selected").addClass("card-subscription");
@@ -110,12 +122,14 @@ $(document).ready(function () {
         if (checked_status == true) {
             $("#submit-subscription_7").removeAttr("disabled");
             $("#checkbox_2, #checkbox_3, #checkbox_4, #checkbox_5, #checkbox_6, #checkbox_1, #checkbox_8").attr("disabled", "disabled");
+            $("#checkbox_2, #checkbox_3, #checkbox_4, #checkbox_5, #checkbox_6, #checkbox_1, #checkbox_8").css('cursor', 'not-allowed');
             $('#subscription_choice').text('Abonament 6 luni pe 2+ dispozitive:');
             $("#price_1, #price_2").text('€72.00 / 6 luni');
             $("#card_7").removeClass("card-subscription").addClass("card-subscription-selected");
         } else {
             $("#submit-subscription_7").attr("disabled", "disabled");
             $("#checkbox_2, #checkbox_3, #checkbox_4, #checkbox_5, #checkbox_6, #checkbox_1, #checkbox_8").removeAttr("disabled");
+            $("#checkbox_2, #checkbox_3, #checkbox_4, #checkbox_5, #checkbox_6, #checkbox_1, #checkbox_8").css('cursor', 'pointer');
             $('#subscription_choice').text('Selecteaza un abonament:');
             $("#price_1, #price_2").text('€0.00 / luna');
             $("#card_7").removeClass("card-subscription-selected").addClass("card-subscription");
@@ -127,12 +141,14 @@ $(document).ready(function () {
         if (checked_status == true) {
             $("#submit-subscription_8").removeAttr("disabled");
             $("#checkbox_2, #checkbox_3, #checkbox_4, #checkbox_5, #checkbox_6, #checkbox_7, #checkbox_1").attr("disabled", "disabled");
+            $("#checkbox_2, #checkbox_3, #checkbox_4, #checkbox_5, #checkbox_6, #checkbox_7, #checkbox_1").css('cursor', 'not-allowed');
             $('#subscription_choice').text('Abonament 12 luni pe 2+ dispozitive:');
             $("#price_1, #price_2").text('€120.00 / 12 luni');
             $("#card_8").removeClass("card-subscription").addClass("card-subscription-selected");
         } else {
             $("#submit-subscription_8").attr("disabled", "disabled");
             $("#checkbox_2, #checkbox_3, #checkbox_4, #checkbox_5, #checkbox_6, #checkbox_7, #checkbox_1").removeAttr("disabled");
+            $("#checkbox_2, #checkbox_3, #checkbox_4, #checkbox_5, #checkbox_6, #checkbox_7, #checkbox_1").css('cursor', 'pointer');
             $('#subscription_choice').text('Selecteaza un abonament:');
             $("#price_1, #price_2").text('€0.00 / luna');
             $("#card_8").removeClass("card-subscription-selected").addClass("card-subscription");
