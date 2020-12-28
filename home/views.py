45e3f1bc0bfbad2c363dea9ed3b19d2f18898348
iulianpro <iulian.prodea@gmail.com>
2020-12-28 13:01:02 +0000

    added aubout us page

--- home/views.py
+++ home/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 
 
 def index(request):
-    """ A view to return the index page """
-    
     return render(request, 'home/index.html')
+
+
+def about_us(request):
+    return render(request, 'home/about-us.html')
