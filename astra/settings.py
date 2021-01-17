cc83eaf10e49f89f96a9f01d4a79e43950470bb8
iulianpro <iulian.prodea@gmail.com>
2021-01-17 18:54:52 +0000

    error fixed

--- astra/settings.py
+++ astra/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['127.0.0.1', 'iulianpro-astra.herpkuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'iulianpro-astra.herokuapp.com']
 
 
 # Application definition
