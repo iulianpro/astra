1ba4689ba303c606fbfb6bc6ae8139580ec97cd9
iulianpro <iulian.prodea@gmail.com>
2021-01-18 19:43:00 +0000

    updated backend email settings

--- astra/settings.py
+++ astra/settings.py
@@ -208,7 +208,7 @@ if 'DEVELOPMENT' in os.environ:
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_HOST = os.getenv('EMAIL_HOST')
-    EMAIL_PORT = 465
+    EMAIL_PORT = 587
     EMAIL_USE_SSL = True
     DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
     EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
